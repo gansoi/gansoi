d84de6e1238cd9f0daa3f50afcd5f8d82b2050d4
Anders Brander <anders@brander.dk>
2016-09-07 08:24:48 +0200

    Added ListAgents() to list all agents for helping when creating a GUI for adding checks.

--- agents/Agent.go
+++ agents/Agent.go
@@ -1,12 +1,30 @@
 package agents
 
-import "reflect"
+import (
+	"reflect"
+	"strings"
+)
 
 type (
 	// Agent should be implemented by all agents.
 	Agent interface {
 		// Check should run the agents check.
 		Check() (interface{}, error)
+	}
+
+	// ArgumentDescription contains everything needed for creating a GUI for
+	// configuring an agent.
+	ArgumentDescription struct {
+		Name        string   `json:"name"`
+		Type        string   `json:"type"`
+		Description string   `json:"description"`
+		EnumValues  []string `json:"enum"`
+	}
+
+	// AgentDescription describes an agent.
+	AgentDescription struct {
+		Name      string                `json:"name"`
+		Arguments []ArgumentDescription `json:"arguments"`
 	}
 )
 
@@ -27,4 +45,51 @@ func RegisterAgent(name string, agent interface{}) {
 // GetAgent will return an agent registred with the name.
 func GetAgent(name string) Agent {
 	return reflect.New(agents[name]).Interface().(Agent)
+}
+
+// ListAgents will return a list of all agents.
+func ListAgents() []AgentDescription {
+	list := make([]AgentDescription, 0, len(agents))
+
+	for name, typ := range agents {
+		list = append(list, AgentDescription{
+			Name:      name,
+			Arguments: getArguments(typ),
+		})
+	}
+
+	return list
+}
+
+func getArguments(elem reflect.Type) []ArgumentDescription {
+	parameters := []ArgumentDescription{}
+
+	l := elem.NumField()
+
+	for i := 0; i < l; i++ {
+		f := elem.Field(i)
+
+		jsonName := f.Tag.Get("json")
+
+		if f.Anonymous {
+			parameters = append(parameters, getArguments(f.Type)...)
+		} else if jsonName != "" {
+			p := ArgumentDescription{}
+
+			p.Name = jsonName
+			p.Type = f.Type.String()
+			p.Description = f.Tag.Get("description")
+			enum := f.Tag.Get("enum")
+			if enum != "" {
+				p.EnumValues = strings.Split(enum, ",")
+				p.Type = "enum"
+			} else {
+				p.EnumValues = []string{}
+			}
+
+			parameters = append(parameters, p)
+		}
+	}
+
+	return parameters
 }
