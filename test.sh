c70f644a7a4706cd69ccff746e897ca0cfe2086b
Anders Brander <anders@brander.dk>
2016-09-30 20:19:16 +0200

    Don't require sudo for running local development test.

--- test.sh
+++ test.sh
@@ -1,37 +1,35 @@
 #!/bin/sh
 
 cat >/tmp/1.conf <<EOF
-local = "local1.gansoi.com"
+local = "local1.gansoi.com:9002"
 cert = "/etc/gansoi/cert.pem"
 key = "/etc/gansoi/key.pem"
 datadir = "/tmp/gansoi-1"
-cluster = ["local1.gansoi.com", "local2.gansoi.com", "local3.gansoi.com"]
+cluster = ["local1.gansoi.com:9002", "local2.gansoi.com:9002", "local3.gansoi.com:9002"]
 secret = "Qhzw5UTm66dWbHNyBdxazf6LfB7iaqcaRXzoot77gwMLErm1vb3VEDTBTupm8KxH"
 EOF
 
 cat >/tmp/2.conf <<EOF
-local = "local2.gansoi.com"
+local = "local2.gansoi.com:9002"
 cert = "/etc/gansoi/cert.pem"
 key = "/etc/gansoi/key.pem"
 datadir = "/tmp/gansoi-2"
-cluster = ["local1.gansoi.com", "local2.gansoi.com", "local3.gansoi.com"]
+cluster = ["local1.gansoi.com:9002", "local2.gansoi.com:9002", "local3.gansoi.com:9002"]
 secret = "Qhzw5UTm66dWbHNyBdxazf6LfB7iaqcaRXzoot77gwMLErm1vb3VEDTBTupm8KxH"
 EOF
 
 cat >/tmp/3.conf <<EOF
-local = "local3.gansoi.com"
+local = "local3.gansoi.com:9002"
 cert = "/etc/gansoi/cert.pem"
 key = "/etc/gansoi/key.pem"
 datadir = "/tmp/gansoi-3"
-cluster = ["local1.gansoi.com", "local2.gansoi.com", "local3.gansoi.com"]
+cluster = ["local1.gansoi.com:9002", "local2.gansoi.com:9002", "local3.gansoi.com:9002"]
 secret = "Qhzw5UTm66dWbHNyBdxazf6LfB7iaqcaRXzoot77gwMLErm1vb3VEDTBTupm8KxH"
 EOF
 
 mkdir -p /tmp/gansoi-1
 mkdir -p /tmp/gansoi-2
 mkdir -p /tmp/gansoi-3
-
-sudo setcap CAP_NET_BIND_SERVICE=+eip ./gansoi
 
 export DEBUG=*
 
