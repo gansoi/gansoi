fc6571a420026c2de6224656aa1a599ea6b8a81b
Anders Brander <anders@brander.dk>
2017-01-16 00:31:50 +0100

    Added a few BoltStore tests.

--- boltdb/BoltStore_test.go
+++ boltdb/BoltStore_test.go
@@ -3,23 +3,38 @@ package boltdb
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/gansoi/gansoi/database"
+	"github.com/hashicorp/raft"
 )
 
 var (
 	db *BoltStore
 )
 
+const (
+	path = "/dev/shm/gansoi-test.db"
+)
+
 type (
 	TestDb struct {
 		*BoltStore
 	}
+
+	data struct {
+		ID string `storm:"id"`
+		A  string
+	}
 )
+
+func init() {
+	database.RegisterType(data{})
+}
 
 func newTestDb() *TestDb {
 	var err error
-	db, err = NewBoltStore("/dev/shm/gansoi-test.db")
+	db, err = NewBoltStore(path)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -60,4 +75,160 @@ func TestDatabaseOpenFail(t *testing.T) {
 	}
 }
 
+func TestBoltStoreSave(t *testing.T) {
+	db := newTestDb()
+	defer db.clean()
+
+	d := data{
+		ID: "bah",
+		A:  "hello",
+	}
+
+	err := db.Save(&d)
+	if err != nil {
+		t.Fatalf("Save() failed: %s", err.Error())
+	}
+}
+
+func TestBoltStoreOne(t *testing.T) {
+	db := newTestDb()
+	defer db.clean()
+
+	d := data{
+		ID: "bah",
+		A:  "hello",
+	}
+
+	err := db.Save(&d)
+	if err != nil {
+		t.Fatalf("Save() failed: %s", err.Error())
+	}
+
+	err = db.One("ID", "bah", &d)
+	if err != nil {
+		t.Fatalf("One() failed: %s", err.Error())
+	}
+
+	err = db.One("ID", "bah2", &d)
+	if err != database.ErrNotFound {
+		t.Fatalf("One() did not return correct error for record not found, returned: %s", err.Error())
+	}
+}
+
+func TestBoltStoreAll(t *testing.T) {
+	db := newTestDb()
+	defer db.clean()
+
+	d := data{
+		ID: "bah",
+		A:  "hello",
+	}
+
+	var all []data
+	err := db.All(&all, -1, 0, false)
+	if err != nil {
+		t.Fatalf("All() failed: %s", err.Error())
+	}
+
+	if len(all) != 0 {
+		t.Fatalf("All() returned wrong number of results, expected 0, got %d", len(all))
+	}
+
+	err = db.Save(&d)
+	if err != nil {
+		t.Fatalf("Save() failed: %s", err.Error())
+	}
+
+	err = db.All(&all, -1, 0, false)
+	if err != nil {
+		t.Fatalf("All() failed: %s", err.Error())
+	}
+
+	if len(all) != 1 {
+		t.Fatalf("All() returned wrong number of results, expected 1, got %d", len(all))
+	}
+
+	if all[0].ID != d.ID || all[0].A != d.A {
+		t.Fatalf("All() returned wrong result %v != %v", all[0], d)
+	}
+}
+
+func TestBoltStoreStorm(t *testing.T) {
+	db := newTestDb()
+	defer db.clean()
+
+	if db.Storm().Bolt.Path() != path {
+		t.Fatalf("Something is wrong with the underlying Storm/Bolt storage")
+	}
+}
+
+func TestProcessLogEntry(t *testing.T) {
+	db := newTestDb()
+	defer db.clean()
+
+	d := data{
+		ID: "bah",
+		A:  "hello",
+	}
+
+	save := database.NewLogEntry(database.CommandSave, d)
+	err := db.ProcessLogEntry(save)
+	if err != nil {
+		t.Fatalf("ProcessLogEntry() failed: %s", err.Error())
+	}
+
+	err = db.One("ID", "bah", &d)
+	if err != nil {
+		t.Fatalf("One() failed: %s", err.Error())
+	}
+
+	del := database.NewLogEntry(database.CommandDelete, d)
+	err = db.ProcessLogEntry(del)
+	if err != nil {
+		t.Fatalf("ProcessLogEntry() failed: %s", err.Error())
+	}
+
+	err = db.One("ID", "bah", &d)
+	if err == nil {
+		t.Fatalf("CommandDelete failed to delete")
+	}
+
+	broken := database.NewLogEntry(76, d)
+	err = db.ProcessLogEntry(broken)
+	if err == nil {
+		t.Fatalf("ProcessLogEntry() failed to detect unknown command")
+	}
+
+	// Give the store a chance to run goroutines.
+	time.Sleep(time.Millisecond * 100)
+}
+
+func TestBoltStoreApply(t *testing.T) {
+	db := newTestDb()
+	defer db.clean()
+
+	d := data{
+		ID: "bah",
+		A:  "hello",
+	}
+
+	entry := database.NewLogEntry(database.CommandSave, &d)
+	l := &raft.Log{}
+	l.Type = raft.LogCommand
+
+	ret := db.Apply(l)
+	if ret != nil {
+		t.Fatalf("Apply() failed to recognize broken log entry")
+	}
+
+	l.Data = entry.Byte()
+	ret = db.Apply(l)
+
+	err, ok := ret.(error)
+	if ok && err != nil {
+		t.Fatalf("Apply() failed: %s", err.Error())
+	}
+}
+
+// Make sure we implement the needed interface.
 var _ database.Database = (*BoltStore)(nil)
