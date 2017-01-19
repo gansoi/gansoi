df61c12ca07fe2213ddd93cbc3a439ee28514093
Anders Brander <anders@brander.dk>
2017-01-19 22:54:04 +0100

    Added a few tests for TCPPort.

--- plugins/agents/tcpport/TCPPort_test.go
+++ plugins/agents/tcpport/TCPPort_test.go
@@ -1,6 +1,7 @@
 package tcpport
 
 import (
+	"net"
 	"testing"
 
 	"github.com/gansoi/gansoi/plugins"
@@ -9,6 +10,50 @@ import (
 func TestAgent(t *testing.T) {
 	a := plugins.GetAgent("tcpport")
 	_ = a.(*TCPPort)
+}
+
+func TestCheckFail(t *testing.T) {
+	a := TCPPort{
+		Address: "127.0.0.1:0",
+	}
+
+	result := plugins.NewAgentResult()
+	err := a.Check(result)
+	if err == nil {
+		t.Fatalf("Failed to detect error")
+	}
+}
+
+func TestCheckV4(t *testing.T) {
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+
+	a := TCPPort{
+		Address: l.Addr().String(),
+	}
+
+	result := plugins.NewAgentResult()
+	err = a.Check(result)
+	if err != nil {
+		t.Fatalf("Check failed: %s", err.Error())
+	}
+
+	l.Close()
+}
+
+func TestCheckV6(t *testing.T) {
+	l, err := net.Listen("tcp6", "[::1]:0")
+
+	a := TCPPort{
+		Address: l.Addr().String(),
+	}
+
+	result := plugins.NewAgentResult()
+	err = a.Check(result)
+	if err != nil {
+		t.Fatalf("Check failed: %s", err.Error())
+	}
+
+	l.Close()
 }
 
 var _ plugins.Agent = (*TCPPort)(nil)
