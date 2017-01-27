7da789c65b871c88317884e8c1558fb35695e0a9
Anders Brander <anders@brander.dk>
2017-01-27 18:30:51 +0100

    Report ConnectDuration as milliseconds.

--- plugins/agents/tcpport/TCPPort.go
+++ plugins/agents/tcpport/TCPPort.go
@@ -25,7 +25,7 @@ func (t *TCPPort) Check(result plugins.AgentResult) error {
 	}
 
 	// Measure the duration. This is the only check we for for now.
-	result.AddValue("ConnectDuration", time.Now().Sub(start))
+	result.AddValue("ConnectDuration", ms(time.Now().Sub(start)))
 
 	// It doesn't make sense to measure close timing. Go returns without error
 	// before the remote end acks.
@@ -35,4 +35,9 @@ func (t *TCPPort) Check(result plugins.AgentResult) error {
 	}
 
 	return nil
+}
+
+// ms will convert a time.Duration to milliseconds.
+func ms(d time.Duration) int64 {
+	return ((d + time.Millisecond/2) / time.Millisecond).Nanoseconds()
 }
