d58204183b5c7d108ce0f75e6a3c30322eef3928
Anders Brander <anders@brander.dk>
2017-02-16 05:26:43 +0100

    Test console output.

--- plugins/notifiers/console/Console_test.go
+++ plugins/notifiers/console/Console_test.go
@@ -1,6 +1,8 @@
 package console
 
 import (
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/gansoi/gansoi/plugins"
@@ -9,6 +11,26 @@ import (
 func TestNotifier(t *testing.T) {
 	n := plugins.GetNotifier("console")
 	_ = n.(*Console)
+}
+
+func TestNotify(t *testing.T) {
+	real := os.Stdout
+	r, w, _ := os.Pipe()
+
+	c := plugins.GetNotifier("console")
+
+	os.Stdout = w
+	c.Notify("hellohello")
+	os.Stdout = real
+
+	buf := make([]byte, 1024)
+	n, _ := r.Read(buf)
+
+	w.Close()
+
+	if !strings.Contains(string(buf[:n]), "hellohello") {
+		t.Fatalf("Output did nto contain our log entry")
+	}
 }
 
 var _ plugins.Notifier = (*Console)(nil)
