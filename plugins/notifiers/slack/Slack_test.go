c73b858553bbd6fb2ca1999f6e8d07649ce2e69e
Anders Brander <anders@brander.dk>
2017-07-10 10:07:25 +0200

    Added a few Slack testcases.

--- plugins/notifiers/slack/Slack_test.go
+++ plugins/notifiers/slack/Slack_test.go
@@ -11,4 +11,23 @@ func TestNotifier(t *testing.T) {
 	_ = n.(*Slack)
 }
 
+func TestSlackFail(t *testing.T) {
+	n := plugins.GetNotifier("slack")
+	err := n.Notify("hello")
+	if err == nil {
+		t.Fatalf("Notify() did not return an error for empty arguments")
+	}
+}
+
+func TestSlack(t *testing.T) {
+	s := Slack{
+		URL: "https://hooks.slack.com/services/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",
+	}
+
+	err := s.Notify("TestSlack")
+	if err != nil {
+		t.Fatalf("Notify() returned an error: %s", err.Error())
+	}
+}
+
 var _ plugins.Notifier = (*Slack)(nil)
