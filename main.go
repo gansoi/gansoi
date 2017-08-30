9eec1004aa001caf7c2c61c432ffd45f4cdf8f59
Anders Brander <anders@brander.dk>
2017-08-30 22:11:05 +0200

    Added a few tests of the main package.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
@@ -47,6 +48,12 @@ import (
 
 var (
 	configFile = config.DefaultPath
+
+	// These can be overridden when testing.
+	exit   = os.Exit
+	stdin  = io.Reader(os.Stdin)
+	stdout = io.Writer(os.Stdout)
+	stderr = io.Writer(os.Stderr)
 )
 
 func init() {
@@ -62,7 +69,7 @@ func init() {
 func bailIfError(err error) {
 	if err != nil {
 		logger.Info("main", err.Error())
-		os.Exit(1)
+		exit(1)
 	}
 }
 
@@ -142,31 +149,39 @@ func main() {
 		},
 	}
 
+	// This is added to be able to call this function without any output
+	// from tests.
+	hidden := &cobra.Command{
+		Use:    "hidden",
+		Hidden: true,
+	}
+
 	var rootCmd = &cobra.Command{Use: os.Args[0]}
 	rootCmd.AddCommand(cmdCore)
 	rootCmd.AddCommand(cmdCheck)
 	rootCmd.AddCommand(nagCheck)
+	rootCmd.AddCommand(hidden)
 	rootCmd.Execute()
 }
 
 func runCheck(printSummary bool, arguments []string) {
 	var err error
 	var check checks.Check
-	f := os.Stdin
+	f := stdin
 	if len(arguments) > 0 {
 		f, err = os.Open(arguments[0])
 		if err != nil {
-			os.Stderr.WriteString(err.Error())
+			stderr.Write([]byte(err.Error()))
 
-			os.Exit(3)
+			exit(3)
 		}
 	}
 
 	in, err := ioutil.ReadAll(f)
 	if err != nil {
-		os.Stderr.WriteString(err.Error())
+		stderr.Write([]byte(err.Error()))
 
-		os.Exit(3)
+		exit(3)
 	}
 
 	// Input looks like we're called from a hash-bang script. Try to find json
@@ -174,9 +189,9 @@ func runCheck(printSummary bool, arguments []string) {
 	if bytes.HasPrefix(in, []byte("#!")) {
 		start := bytes.IndexRune(in, '{')
 		if start == -1 {
-			os.Stderr.WriteString("Cannot find JSON in input stream")
+			stderr.Write([]byte("Cannot find JSON in input stream"))
 
-			os.Exit(3)
+			exit(3)
 		}
 
 		in = in[start:]
@@ -184,9 +199,9 @@ func runCheck(printSummary bool, arguments []string) {
 
 	err = json.Unmarshal(in, &check)
 	if err != nil {
-		os.Stderr.WriteString(err.Error())
+		stderr.Write([]byte(err.Error()))
 
-		os.Exit(3)
+		exit(3)
 	}
 
 	// FIXME: Support remote hosts here!
@@ -194,20 +209,20 @@ func runCheck(printSummary bool, arguments []string) {
 
 	if printSummary {
 		if result.Error != "" {
-			os.Stdout.WriteString(result.Error + "\n")
+			stdout.Write([]byte(result.Error + "\n"))
 		} else {
-			os.Stdout.WriteString("ok\n")
+			stdout.Write([]byte("ok\n"))
 		}
 	}
 
 	// Pretty-print result as json.
 	out, _ := json.MarshalIndent(result, "", "\t")
-	os.Stdout.Write(out)
+	stdout.Write(out)
 
 	if result.Error != "" {
 		// Nagios and many other monitoring solutions use the exit code 2 to
 		// signal check failure. Let's all agree :)
-		os.Exit(2)
+		exit(2)
 	}
 }
 
@@ -217,7 +232,7 @@ func loadConfig() *config.Configuration {
 	err := conf.LoadFromFile(configFile)
 	if err != nil {
 		logger.Info("main", "Failed to read configuration file at %s: %s", configFile, err.Error())
-		os.Exit(1)
+		exit(1)
 	}
 
 	return &conf
@@ -227,13 +242,13 @@ func openDatabase(conf *config.Configuration) *boltdb.BoltStore {
 	db, err := boltdb.NewBoltStore(path.Join(conf.DataDir, "gansoi.db"))
 	if err != nil {
 		logger.Info("main", "failed to open database in %s: %s", conf.DataDir, err.Error())
-		os.Exit(1)
+		exit(1)
 	}
 
 	return db
 }
 
-func initCore(cmd *cobra.Command, _ []string) {
+func initCore(_ *cobra.Command, _ []string) {
 	conf := loadConfig()
 	info := cluster.NewInfo(path.Join(conf.DataDir, "cluster.json"))
 	core := cluster.NewCore(info)
@@ -253,28 +268,28 @@ func initRunCore(cmd *cobra.Command, arguments []string) {
 }
 
 func joinCore(_ *cobra.Command, arguments []string) {
-	conf := loadConfig()
-	info := cluster.NewInfo(path.Join(conf.DataDir, "cluster.json"))
-	core := cluster.NewCore(info)
-
-	self := cluster.DefaultPort(conf.Bind)
-	info.SetSelf(self)
-
 	// Check that we have all arguments.
 	if len(arguments) < 2 {
 		logger.Info("join", "Too few arguments")
-		os.Exit(1)
+		exit(1)
 	}
 
 	// Split join-token in hash and cluster-token.
 	parts := strings.Split(arguments[1], ".")
 	if len(parts) < 2 {
 		logger.Info("join", "Join-token is malformed")
-		os.Exit(1)
+		exit(1)
 	}
 
 	hash := parts[0]
 	token := parts[1]
+
+	conf := loadConfig()
+	info := cluster.NewInfo(path.Join(conf.DataDir, "cluster.json"))
+	core := cluster.NewCore(info)
+
+	self := cluster.DefaultPort(conf.Bind)
+	info.SetSelf(self)
 
 	err := core.Join(arguments[0], hash, token, conf.Bind)
 	bailIfError(err)
@@ -284,7 +299,7 @@ func printCa(_ *cobra.Command, _ []string) {
 	conf := loadConfig()
 	info := cluster.NewInfo(path.Join(conf.DataDir, "cluster.json"))
 
-	fmt.Printf("%s", info.CACert)
+	stdout.Write(info.CACert)
 }
 
 func printToken(_ *cobra.Command, _ []string) {
@@ -293,11 +308,12 @@ func printToken(_ *cobra.Command, _ []string) {
 
 	hash := sha256.Sum256(info.CACert)
 
-	fmt.Printf("%x.%s\n", hash, info.ClusterToken)
-
-	fmt.Printf("\nCan be used to join a new core node to the cluster like this:\n"+
+	out := fmt.Sprintf("%x.%s\n", hash, info.ClusterToken)
+	out += fmt.Sprintf("\nCan be used to join a new core node to the cluster like this:\n"+
 		"# gansoi core join %s %x.%s\n",
 		info.Self(), hash, info.ClusterToken)
+
+	stdout.Write([]byte(out))
 }
 
 func runCore(_ *cobra.Command, _ []string) {
@@ -331,7 +347,7 @@ func runCore(_ *cobra.Command, _ []string) {
 	n, err := node.NewNode(stream, conf.DataDir, db, db, info, pair, core.CA())
 	if err != nil {
 		logger.Info("main", "%s", err.Error())
-		os.Exit(1)
+		exit(1)
 	}
 	db.RegisterListener(n)
 
@@ -351,7 +367,7 @@ func runCore(_ *cobra.Command, _ []string) {
 				sshErr := ssh.Init(n)
 				if sshErr != nil {
 					logger.Info("main", "ssh.Init() error: %s", sshErr.Error())
-					os.Exit(1)
+					exit(1)
 				}
 
 				scheduler.Run()
@@ -548,7 +564,7 @@ func runCore(_ *cobra.Command, _ []string) {
 
 			if err = lManager.CacheFile(cacheFile); err != nil {
 				logger.Info("main", "Failed to open Letsencrypt cachefile at %s: %s", cacheFile, err.Error())
-				os.Exit(1)
+				exit(1)
 			}
 
 			// ensure we dont ask for random certificates
@@ -567,6 +583,6 @@ func runCore(_ *cobra.Command, _ []string) {
 
 	if err != nil {
 		logger.Info("main", "Bind to %s failed: %s", conf.HTTP.Bind, err.Error())
-		os.Exit(1)
+		exit(1)
 	}
 }
---
+++ main_test.go
@@ -0,0 +1,304 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"path"
+	"testing"
+	"time"
+
+	"github.com/gansoi/gansoi/cluster"
+	"github.com/gansoi/gansoi/config"
+)
+
+type (
+	failReader struct{}
+)
+
+func init() {
+	exit = fakeExit
+}
+
+func (f *failReader) Read(p []byte) (n int, err error) {
+	return 0, errors.New("error")
+}
+
+func fakeExit(code int) {
+	panic(code)
+}
+
+func expectExit(t *testing.T, code int) {
+	if code < 0 {
+		got := recover()
+		if got != nil {
+			t.Fatalf("exit unexpectedly called with value %v", got)
+		}
+
+		return
+	}
+
+	got := recover()
+	if got != code {
+		t.Fatalf("exited with wrong error %d, expected %d", got, code)
+	}
+}
+
+func TestExit(t *testing.T) {
+	defer expectExit(t, 0)
+
+	exit(0)
+}
+
+func TestBailIfError(t *testing.T) {
+	defer expectExit(t, 1)
+
+	bailIfError(errors.New("error"))
+}
+
+func TestBailIfErrorNil(t *testing.T) {
+	defer expectExit(t, -1)
+
+	bailIfError(nil)
+}
+
+func TestCobraSetup(t *testing.T) {
+	os.Args = []string{os.Args[0], "hidden"}
+
+	main()
+}
+
+func TestRunCheckJSONError(t *testing.T) {
+	defer expectExit(t, 3)
+	stderr = ioutil.Discard
+
+	os.Args = []string{os.Args[0], "runcheck"}
+	main()
+}
+
+func TestRunCheckMissingFile(t *testing.T) {
+	defer expectExit(t, 3)
+	stderr = ioutil.Discard
+
+	os.Args = []string{os.Args[0], "runcheck", "/missing-file"}
+	main()
+}
+
+func TestRunCheckReadfail(t *testing.T) {
+	defer expectExit(t, 3)
+
+	stderr = ioutil.Discard
+	stdin = &failReader{}
+
+	os.Args = []string{os.Args[0], "runcheck"}
+	main()
+}
+
+func TestRunCheckBangHashJSONFail(t *testing.T) {
+	defer expectExit(t, 3)
+
+	stdout = ioutil.Discard
+	stderr = ioutil.Discard
+	stdin = bytes.NewBufferString(`#!/gansoi`)
+
+	os.Args = []string{os.Args[0], "runcheck"}
+	main()
+}
+
+func TestRunCheckBangHash(t *testing.T) {
+	listenener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatalf("ListenTCP() failed: %s", err.Error())
+	}
+	defer listenener.Close()
+
+	defer expectExit(t, -1)
+	stdout = ioutil.Discard
+	stderr = ioutil.Discard
+	stdin = bytes.NewBufferString(fmt.Sprintf(`#!/gansoi
+{
+	"agent": "tcpport",
+	"interval": 60000000000,
+	"arguments": {
+		"address": "%s"
+	}
+}`, listenener.Addr().String()))
+
+	os.Args = []string{os.Args[0], "runcheck"}
+	main()
+}
+
+func TestNagiosPluginBangHash(t *testing.T) {
+	listenener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatalf("ListenTCP() failed: %s", err.Error())
+	}
+	defer listenener.Close()
+
+	defer expectExit(t, -1)
+	stdout = ioutil.Discard
+	stderr = ioutil.Discard
+	stdin = bytes.NewBufferString(fmt.Sprintf(`#!/gansoi
+{
+	"agent": "tcpport",
+	"interval": 60000000000,
+	"arguments": {
+		"address": "%s"
+	}
+}`, listenener.Addr().String()))
+
+	os.Args = []string{os.Args[0], "nagiosplugin"}
+	main()
+}
+
+func TestNagiosPluginBangHashFailed(t *testing.T) {
+	defer expectExit(t, 2)
+	stdout = ioutil.Discard
+	stderr = ioutil.Discard
+	stdin = bytes.NewBufferString(`#!/gansoi
+{
+	"agent": "tcpport",
+	"interval": 60000000000,
+	"arguments": {
+		"address": "127.0.0.1:0"
+	}
+}`)
+
+	os.Args = []string{os.Args[0], "nagiosplugin"}
+	main()
+}
+
+func TestLoadConfig(t *testing.T) {
+	f, _ := ioutil.TempFile("/dev/shm", "TestLoadConfig")
+	f.WriteString("")
+	f.Close()
+	defer os.Remove(f.Name())
+
+	configFile = f.Name()
+	conf := loadConfig()
+	if conf == nil {
+		t.Errorf("loadConfig() returned nil")
+	}
+}
+
+func TestLoadConfigFail(t *testing.T) {
+	defer expectExit(t, 1)
+
+	configFile = "/does/not/exist"
+	loadConfig()
+}
+
+func TestOpenDatabase(t *testing.T) {
+	dirname, _ := ioutil.TempDir("/dev/shm/", "TestOpenDatabase")
+	defer os.RemoveAll(dirname)
+
+	conf := &config.Configuration{
+		DataDir: dirname,
+	}
+
+	store := openDatabase(conf)
+	store.Close()
+}
+
+func TestOpenDatabaseFail(t *testing.T) {
+	conf := &config.Configuration{
+		DataDir: "/does/not/exist",
+	}
+
+	defer expectExit(t, 1)
+
+	store := openDatabase(conf)
+	store.Close()
+}
+
+func TestInitCoreFail(t *testing.T) {
+	defer expectExit(t, 1)
+
+	initCore(nil, nil)
+}
+
+func TestInitRunCoreFail(t *testing.T) {
+	defer expectExit(t, 1)
+
+	initRunCore(nil, nil)
+}
+
+func TestJoinCore(t *testing.T) {
+	p1 := "/dev/shm/node1"
+	// To test join, we must first start a working node (!)
+	os.Mkdir(p1, 0700)
+	defer os.RemoveAll(p1)
+
+	c1 := `bind = "127.0.0.1:5121"
+datadir = "/dev/shm/node1"
+[http]
+bind = "127.0.0.1:7121"
+tls = false
+`
+	// set node 1 config.
+	configFile = path.Join(p1, "gansoi.conf")
+	ioutil.WriteFile(configFile, []byte(c1), 0666)
+
+	initCore(nil, nil)
+
+	// Retrieve join-token.
+	conf := loadConfig()
+	info := cluster.NewInfo(path.Join(conf.DataDir, "cluster.json"))
+	hash := sha256.Sum256(info.CACert)
+	token := fmt.Sprintf("%x.%s", hash, info.ClusterToken)
+
+	go runCore(nil, nil)
+	time.Sleep(time.Second * 1)
+
+	joinCore(nil, []string{"127.0.0.1:5121", token})
+}
+
+func TestJoinCoreFailNoToken(t *testing.T) {
+	defer expectExit(t, 1)
+
+	joinCore(nil, []string{"127.0.0.1:0"})
+}
+
+func TestJoinCoreFailInvalidToken(t *testing.T) {
+	defer expectExit(t, 1)
+
+	joinCore(nil, []string{"127.0.0.1:0", "no:dot"})
+}
+
+func TestPrintCAFail(t *testing.T) {
+	defer expectExit(t, 1)
+
+	printCa(nil, nil)
+}
+
+func TestMainHTTP(t *testing.T) {
+	p1 := "/dev/shm/TestMainHTTP"
+	os.Mkdir(p1, 0700)
+	defer os.RemoveAll(p1)
+
+	c1 := `bind = "127.0.0.1:5184"
+datadir = "/dev/shm/TestMainHTTP"
+[http]
+bind = "127.0.0.1:5185"
+tls = false
+`
+	// set node 1 config.
+	configFile = path.Join(p1, "gansoi.conf")
+	ioutil.WriteFile(configFile, []byte(c1), 0644)
+
+	initCore(nil, nil)
+	go runCore(nil, nil)
+	time.Sleep(time.Second * 1)
+
+	// This could be MUCH better.
+	http.Get("http://127.0.0.1:5185/api/agents")
+	http.Get("http://127.0.0.1:5185/api/notifiers")
+	http.Get("http://127.0.0.1:5185/api/backup")
+	http.Get("http://127.0.0.1:5185/api/backup.gz")
+	http.Get("http://127.0.0.1:5185/ssh/pubkey")
+}
