b7ae521d3490ce22b41a4da5ee593df005230b4c
Anders Brander <abk@cego.dk>
2017-11-30 07:44:07 +0100

    Added more tests for cluster.Info.

--- cluster/Info_test.go
+++ cluster/Info_test.go
@@ -72,3 +72,64 @@ func TestInfoLoadFail(t *testing.T) {
 		t.Fatalf("Load() failed to detect broken JSON")
 	}
 }
+
+func TestSelf(t *testing.T) {
+	const self = "name-for-testing"
+	info := newInfo()
+	defer rmInfo()
+
+	info.SetSelf(self)
+	if info.Self() != self {
+		t.Errorf("Wrong name returned from Self(), got %s", info.Self())
+	}
+}
+
+func TestIP(t *testing.T) {
+	info := newInfo()
+	defer rmInfo()
+
+	cases := []struct {
+		in  string
+		out string
+	}{
+		{"go-test-localhost-v6.gansoi-dev.com", "::1"},
+		{"go-test-localhost-v4.gansoi-dev.com", "127.0.0.1"},
+		{"go-test-nonexisting.gansoi-dev.com", ""},
+	}
+
+	for _, c := range cases {
+		info.SetSelf(c.in)
+		ip := info.IP()
+
+		if ip == nil && c.out == "" {
+			continue
+		}
+
+		if ip == nil && c.out != "" {
+			t.Errorf("IP() returned wrong IP for '%s', expected '%s', got nil", c.in, c.out)
+			continue
+		}
+
+		if ip.String() != c.out {
+			t.Errorf("IP('%s') returned '%s', expected '%s'", c.in, ip.String(), c.out)
+		}
+	}
+}
+
+func TestDefaultPort(t *testing.T) {
+	cases := []struct {
+		in  string
+		out string
+	}{
+		{"", ":4934"},
+		{"hello", "hello:4934"},
+		{"hello:12333", "hello:12333"},
+	}
+
+	for _, c := range cases {
+		result := DefaultPort(c.in)
+		if result != c.out {
+			t.Errorf("DefaultPort('%s') returned '%s', was expecting '%s'", c.in, result, c.out)
+		}
+	}
+}
